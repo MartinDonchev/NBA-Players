b60acfdda1f4aaacc001e94a8b95ec4d0af5b644
MartinDonchev <donchev_m@abv.bg>
2019-11-02 00:05:21 +0200

    Console app

---
+++ NBA/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NBA
+{
+    class Player
+    {
+        public string Name { get; set; }
+
+        public int PlayerSince { get; set; }
+
+        public string Position { get; set; }
+
+        public double Rating { get; set; }
+    }
+}
---
+++ NBA/Program.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace NBA
+{    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var strResultJason = File.ReadAllText("../../../players.json");
+            var resultPlayer = JsonConvert.DeserializeObject<Player[]>(strResultJason);
+
+            Console.Write("Enter years to qualify: ");
+            var maxYearPlayed = int.Parse(Console.ReadLine());
+            Console.Write("Enter rating: ");
+            var minRateToQualify = int.Parse(Console.ReadLine());
+            var yearNow = DateTime.Today;
+
+            var yearsPlayed = yearNow.Year - maxYearPlayed;
+
+            StringBuilder csvcontent = new StringBuilder();
+            csvcontent.AppendLine("Name,Rating");
+            string csvpath = "F:\\MentorMate\\NBA\\NBA\\player.csv";
+
+
+            foreach (var item in resultPlayer.OrderByDescending(x => x.Rating).ThenBy(x => x.Name))
+            {
+                if (item.PlayerSince > yearsPlayed && item.Rating > minRateToQualify)
+                {
+                    //Console.WriteLine($"{item.Name}, {item.Rating:f1}");
+                    csvcontent.AppendLine($"{item.Name}, {item.Rating}");
+                }
+                
+            }
+
+            File.AppendAllText(csvpath, csvcontent.ToString());                                  
+
+        }
+    }
+}
